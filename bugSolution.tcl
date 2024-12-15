0e0f3de91b54b26157dc4d7d64517efab7436965
BugHunter-X <askzenai@gmail.com>
2024-12-15 02:08:04 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,1 @@
+proc goodproc {a b} {if {[string is double -strict $a] && [string is double -strict $b]} {return [expr {$a + $b}]} else {return "Error: Arguments must be numbers"}} puts [goodproc 10 20] puts [goodproc a 20]
